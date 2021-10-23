51de35b40f7a777d01fa4006f41ba268c2042b3b
Maxwell Mullin <mullinmax@gmail.com>
2021-10-23 15:50:21 -0400

    .

---
+++ main.py
@@ -0,0 +1,21 @@
+import urequests
+
+def wget(url):
+    response = urequests.get(url)
+    response.json()
+
+
+
+
+
+
+def main():
+
+    wget('')
+
+    with open(main.py,'w') as f:
+        
+
+
+if __name__ == '__main__':
+    main()
