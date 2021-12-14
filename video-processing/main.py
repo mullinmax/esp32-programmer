cc849ba5c495c81e41764d043a2c7cd80281b268
Maxwell Mullin <mullinmax@gmail.com>
2021-12-13 23:17:45 -0500

    updated outline

--- video-processing/main.py
+++ video-processing/main.py
@@ -9,10 +9,15 @@ def main():
     num_frames = len(locator_pattern(0))
     a = animation(config.fps, num_frames, locator, config.led_vectors, config.led_pin)
 
-    # calculate expected frames per shift
-    # get sample frames
-    # render animation
-    # find pixels that match animation pixels
-    # find center of pixels for each led
+    # calculate expected frame-shift pattern
+    
+    # find phase offset of shifts to pattern
+
+    # get sample frames from median of each shift
+
+    # find pixels that match animation pixels (regions/bluring?)
+
+    # find center of mass of pixels that match each led
+    
     # output x-y coordinates for each pixel
     
